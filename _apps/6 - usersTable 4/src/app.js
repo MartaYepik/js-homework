34a474d6053b8db97e78b23c11cac3c161736394
Marta Yepik <martayepikm@gmail.com>
2022-12-17 19:40:32 +0200

    modified and sorted date

---
+++ _apps/6 - usersTable 4/src/app.js
@@ -0,0 +1,115 @@
+"use strict";
+import { headings, users } from "./data.js";
+
+const headerTable = document.querySelector("#headerTable");
+const bodyTable = document.querySelector("#tbody");
+let colIndex = -1;
+
+const setHeader = headings
+  .map(
+    (elem) =>
+      `<th 
+    scope="col"
+    class="text-sm font-medium text-gray-900 px-6 py-4 text-left bg-red-700 select-none cursor-pointer"
+    data-type=${elem.dataType}
+    >
+    ${elem.value}
+    </th>`
+  )
+  .join("");
+let userId = 0;
+
+const setTableBody = users
+  .map(
+    (elem) =>
+      `
+    <tr class="bg-gray-200 border-b cursor-pointer even:bg-slate-50 hover:bg-cyan-500">
+    <td
+    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+    >
+    ${userId += 1} 
+    </td>
+    <td
+    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${elem.profile.name} 
+    </td>
+    <td
+    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${elem.email} 
+    </td>
+    <td
+    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${elem.profile.dob} 
+    </td>
+    <td
+    class=" text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap  hover:animated-ping">
+    <a href="https://www.latlong.net/c/?lat=${elem.profile.location.lat}&long=${elem.profile.location.long}" target="_blank" class="flex">
+    ${elem.profile.company}
+     <svg xmlns="http://www.w3.org/2000/svg" 
+    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+</svg>
+
+</a>
+
+    </td>
+    <td
+    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${new Date(elem.createdAt).toLocaleDateString('en-us', { year:"numeric", month:"numeric", day:"numeric", time: "numeric", hour: '2-digit', minute: '2-digit', timeZone: 'UTC' })} 
+    </td>
+    </tr>`
+  )
+  .join("");
+
+headerTable.innerHTML = setHeader;
+bodyTable.innerHTML = setTableBody;
+
+const sortTable = (colNum, type, id, isSorted) => {
+  let elem = document.getElementById(id);
+  let tbody = elem.querySelector('tbody');
+  let rowsArray = Array.from(tbody.rows);
+  let compare;
+
+  switch (type) {
+    case 'number':
+      compare = (rowA, rowB) => {
+        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+      };
+      break;
+    case 'string':
+      compare = (rowA, rowB) => {
+        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+      };
+      break;
+    case 'date':
+      compare = (rowA, rowB) => {
+        return new Date(rowB.cells[colNum].innerHTML) - new Date(rowA.cells[colNum].innerHTML);
+      };
+      break;
+  }
+  
+  rowsArray.sort(compare);
+  if (isSorted) rowsArray.reverse();
+  tbody.append(...rowsArray);
+};
+
+headerTable.onclick = (e) => {
+  if (e.target.tagName !== "TH") return;
+
+  //делегування подій
+  const th = e.target;
+  const index = th.cellIndex;
+  //! Властивість cellIndex повертає позицію клітинки в колекції клітинок рядка таблиці.
+  // у таблиці 6 позицій, повертає значення від 0 до 5
+  const type = th.dataset.type; // dataset для HTML атрибутів
+
+  sortTable(index, type, 'table', colIndex === index);
+  // index (cell number) | type (data-type) | 'table' (id) |  colIndex (false/true)
+  colIndex = colIndex === index ? -1 : index;
+}
+
---
+++ _apps/6 - usersTable 4/src/data.js
@@ -0,0 +1,286 @@
+export let headings = [
+  {
+    key: "userId",
+    value: "#",
+    dataType: "number",
+  },
+  {
+    key: "fullName",
+    value: "Full name",
+    dataType: "string",
+  },
+  {
+    key: "emailAddress",
+    value: "Email",
+    dataType: "string",
+  },
+  {
+    key: "born",
+    value: "Born",
+    dataType: "string",
+  },
+  {
+    key: "company",
+    value: "Company",
+    dataType: "string",
+  },
+  {
+    key: "createAt",
+    value: "Creat At",
+    dataType: "date",
+  },
+];
+
+export const users = [
+   {
+    "id": "63921a5f03385d52390ee373",
+    "email": "phoebe_moore@vicon.everbank",
+    "username": "phoebe93",
+    "profile": {
+      "name": "Phoebe Moore",
+      "company": "Vicon",
+      "dob": "1993-08-09",
+      "address": "57 Cadman Plaza, Nescatunga, Connecticut",
+      "location": {
+        "lat": -48.943804,
+        "long": -83.813714
+      },
+      "about": "Anim quis sit adipisicing id ea aliqua sit enim cillum sint aliqua in laboris consectetur. Culpa sint quis quis minim irure laboris id consectetur pariatur aute ut excepteur."
+    },
+    "apiKey": "ceb03eeb-0c68-496b-b9c3-19e9964c093e",
+    "roles": [
+      "member",
+      "admin"
+    ],
+    "createdAt": "2010-10-11T06:29:21.624Z",
+    "updatedAt": "2010-10-12T06:29:21.624Z"
+  },
+  {
+    "id": "63921a5fef54fdf7b06342f5",
+    "email": "hernandez_parker@pearlessa.investments",
+    "username": "hernandez91",
+    "profile": {
+      "name": "Hernandez Parker",
+      "company": "Pearlessa",
+      "dob": "1991-10-23",
+      "address": "22 Court Square, Dundee, Minnesota",
+      "location": {
+        "lat": 55.318655,
+        "long": 110.473673
+      },
+      "about": "Laboris id in nisi amet consectetur cillum voluptate adipisicing id quis. Et fugiat aliqua sit mollit incididunt."
+    },
+    "apiKey": "2587a366-4906-4918-aae9-2489b128e73a",
+    "roles": [
+      "owner",
+      "member"
+    ],
+    "createdAt": "2011-08-30T19:11:33.878Z",
+    "updatedAt": "2011-08-31T19:11:33.878Z"
+  },
+  {
+    "id": "63921a5f31a75baf4d43761a",
+    "email": "mack_delaney@cosmetex.globo",
+    "username": "mack93",
+    "profile": {
+      "name": "Mack Delaney",
+      "company": "Cosmetex",
+      "dob": "1993-10-23",
+      "address": "4 Bowery Street, Sharon, Wisconsin",
+      "location": {
+        "lat": 56.673801,
+        "long": 101.154213
+      },
+      "about": "Reprehenderit aliqua proident ipsum dolore eiusmod elit duis in ex. Tempor aliqua Lorem exercitation labore culpa labore amet labore non."
+    },
+    "apiKey": "6d2e1385-71f1-47e5-90ad-b217024e2905",
+    "roles": [
+      "owner"
+    ],
+    "createdAt": "2012-08-26T15:44:18.235Z",
+    "updatedAt": "2012-08-27T15:44:18.235Z"
+  },
+  {
+    "id": "63921a5f5d65e6443b005c6b",
+    "email": "maryellen_barnes@terragen.men",
+    "username": "maryellen90",
+    "profile": {
+      "name": "Maryellen Barnes",
+      "company": "Terragen",
+      "dob": "1990-08-12",
+      "address": "24 Ridgecrest Terrace, Hessville, Alaska",
+      "location": {
+        "lat": -58.260827,
+        "long": -175.470199
+      },
+      "about": "Voluptate anim tempor ex fugiat laboris nisi commodo. Enim aute consectetur ex culpa Lorem ipsum."
+    },
+    "apiKey": "5adffbb5-d529-4c82-be5d-f294eb8ee377",
+    "roles": [
+      "admin",
+      "member"
+    ],
+    "createdAt": "2010-10-13T02:11:36.954Z",
+    "updatedAt": "2010-10-14T02:11:36.954Z"
+  },
+  {
+    "id": "63921a5f066d173b5f7b96b5",
+    "email": "carolyn_puckett@quantalia.hamburg",
+    "username": "carolyn88",
+    "profile": {
+      "name": "Carolyn Puckett",
+      "company": "Quantalia",
+      "dob": "1988-01-05",
+      "address": "94 Guernsey Street, Freeburn, Oregon",
+      "location": {
+        "lat": 82.840607,
+        "long": 149.734518
+      },
+      "about": "Ipsum occaecat Lorem proident esse do adipisicing sunt ex eiusmod. Ut aute sunt ut deserunt quis."
+    },
+    "apiKey": "2b83d9f3-f6ed-4e98-8ca6-81e22377ebc0",
+    "roles": [
+      "admin",
+      "guest"
+    ],
+    "createdAt": "2013-05-20T13:45:13.508Z",
+    "updatedAt": "2013-05-21T13:45:13.508Z"
+  },
+  {
+    "id": "63921a5fc705dcb243e346bd",
+    "email": "geraldine_bowen@prismatic.mv",
+    "username": "geraldine88",
+    "profile": {
+      "name": "Geraldine Bowen",
+      "company": "Prismatic",
+      "dob": "1988-08-21",
+      "address": "100 Cortelyou Road, Lavalette, Arkansas",
+      "location": {
+        "lat": 5.406268,
+        "long": -173.131463
+      },
+      "about": "Do consequat reprehenderit incididunt ad do eiusmod officia. Veniam ipsum dolor enim laborum."
+    },
+    "apiKey": "34ed5b48-deab-4999-8764-f99f3a841e26",
+    "roles": [
+      "admin",
+      "member"
+    ],
+    "createdAt": "2013-08-07T00:30:09.158Z",
+    "updatedAt": "2013-08-08T00:30:09.158Z"
+  },
+  {
+    "id": "63921a5fa9d0395d82f8b73c",
+    "email": "manuela_bradley@musanpoly.international",
+    "username": "manuela92",
+    "profile": {
+      "name": "Manuela Bradley",
+      "company": "Musanpoly",
+      "dob": "1992-10-10",
+      "address": "89 Scott Avenue, Catharine, Ohio",
+      "location": {
+        "lat": 83.144922,
+        "long": -114.710915
+      },
+      "about": "Lorem aliqua dolore amet in. Consectetur aute et id id in elit cillum anim."
+    },
+    "apiKey": "585db1a1-e3ec-4e59-b96b-c52277ac4ca1",
+    "roles": [
+      "admin"
+    ],
+    "createdAt": "2014-06-21T05:22:03.938Z",
+    "updatedAt": "2014-06-22T05:22:03.938Z"
+  },
+  {
+    "id": "63921a5f85871f13ca50ee06",
+    "email": "anthony_george@eclipto.bs",
+    "username": "anthony92",
+    "profile": {
+      "name": "Anthony George",
+      "company": "Eclipto",
+      "dob": "1992-02-06",
+      "address": "47 Little Street, Konterra, Guam",
+      "location": {
+        "lat": -79.698025,
+        "long": 34.827819
+      },
+      "about": "Mollit do dolore laboris non magna amet occaecat et tempor sit esse. In sit nulla id ut officia in consequat dolore."
+    },
+    "apiKey": "7a107d9a-2723-4e00-8f01-94ebfcb6e5e6",
+    "roles": [
+      "owner",
+      "admin"
+    ],
+    "createdAt": "2010-11-18T08:55:48.430Z",
+    "updatedAt": "2010-11-19T08:55:48.430Z"
+  },
+  {
+    "id": "63921a5fa2a2bb6a1605325f",
+    "email": "morgan_fields@hotcakes.forex",
+    "username": "morgan88",
+    "profile": {
+      "name": "Morgan Fields",
+      "company": "Hotcakes",
+      "dob": "1988-03-09",
+      "address": "52 Allen Avenue, Northchase, Rhode Island",
+      "location": {
+        "lat": -73.376605,
+        "long": 145.125036
+      },
+      "about": "Aute ullamco duis labore ullamco. Reprehenderit minim proident proident pariatur irure consequat anim aute exercitation velit laborum deserunt velit quis."
+    },
+    "apiKey": "d0907d98-4a42-4be9-9c1b-1f10fa652e5a",
+    "roles": [
+      "guest",
+      "member"
+    ],
+    "createdAt": "2010-02-07T18:03:25.385Z",
+    "updatedAt": "2010-02-08T18:03:25.385Z"
+  },
+  {
+    "id": "63921a5f46cabaac396f8636",
+    "email": "howe_boyer@zidant.engineering",
+    "username": "howe90",
+    "profile": {
+      "name": "Howe Boyer",
+      "company": "Zidant",
+      "dob": "1990-05-12",
+      "address": "51 Colin Place, Leola, Virgin Islands",
+      "location": {
+        "lat": 12.244276,
+        "long": -140.079929
+      },
+      "about": "Occaecat sit elit adipisicing exercitation ad elit consectetur nulla velit veniam laboris minim. Occaecat veniam esse cupidatat ex non quis fugiat cupidatat aute."
+    },
+    "apiKey": "a3d42ba2-ccf7-4e32-b18c-8dd538743e3d",
+    "roles": [
+      "owner",
+      "guest"
+    ],
+    "createdAt": "2013-08-15T09:46:48.702Z",
+    "updatedAt": "2013-08-16T09:46:48.702Z"
+  },
+  {
+    "id": "63921a5f46cabaac396f8636",
+    "email": "howe_boyer@zidant.engineering",
+    "username": "howe90",
+    "profile": {
+      "name": "Howe Boyer",
+      "company": "Zidant",
+      "dob": "1990-03-12",
+      "address": "51 Colin Place, Leola, Virgin Islands",
+      "location": {
+        "lat": 12.244276,
+        "long": -140.079929
+      },
+      "about": "Occaecat sit elit adipisicing exercitation ad elit consectetur nulla velit veniam laboris minim. Occaecat veniam esse cupidatat ex non quis fugiat cupidatat aute."
+    },
+    "apiKey": "a3d42ba2-ccf7-4e32-b18c-8dd538743e3d",
+    "roles": [
+      "owner",
+      "guest"
+    ],
+    "createdAt": "2013-08-15T09:45:48.702Z",
+    "updatedAt": "2013-08-16T09:46:48.702Z"
+  }
+];
